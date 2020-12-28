5f9854cfe8d5ba43b9661332f40874f894476254
= <=>
2020-12-28 07:31:08 +0000

    changes

--- sync.py
+++ sync.py
@@ -6,6 +6,8 @@ import os
 from shutil import copyfile
 
 
+
+
 def hash_file(path):
     # BUF_SIZE is totally arbitrary, change for your app!
     BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
@@ -19,50 +21,81 @@ def hash_file(path):
 
     return md5.hexdigest()
 
-
-def list_dir_files_with_hash(path, file_listing=None, recursive_folders=True):
-
-    #if no file listing dic then create
-    if not file_listing:
-        file_listing = {}
-
-    #walk all files/dirs in path
-    for file in os.listdir(path):
-        #loop files
-        rel_path = os.path.relpath(path + "/" + file)
-
-        #get extra details
-        fl = pathlib.Path(rel_path)
-        date_modified = datetime.datetime.fromtimestamp(fl.stat().st_mtime)
-        file_size = fl.stat().st_size
-
-        # add file to listing
-        if os.path.isdir(rel_path):
-            type = "folder"
-            hash = None
-        else:
-            type = "files"
-            hash = hash_file(rel_path)
-
-        #add to listing
-        file_listing[rel_path] = [hash, date_modified, file_size, type]
-
-        #if folders recursively loop contents
-        if type == "folder":
-            file_listing = list_dir_files_with_hash(rel_path, file_listing, recursive_folders)
-
-    return file_listing
-
-def sync_folders(source, dest):
-
-    source_files = list_dir_files_with_hash(source)
-    source_files = {k.replace(source,""):v for k,v in source_files.items()}
-    dest_files = list_dir_files_with_hash(dest)
-    dest_files = {k.replace(dest, ""): v for k, v in dest_files.items()}
-
-    pass
-
-
+class Syncer:
+    
+    def __init__(self, sourcePath, destPath):
+        
+        self.sourcePath = sourcePath 
+        self.destPath = destPath
+        self.dest_files = None
+        self.source_files = None
+        self.log = ""
+    def list_dir_files_with_hash(self, path, file_listing=None, recursive_folders=True):
+    
+        #if no file listing dic then create
+        if not file_listing:
+            file_listing = {}
+    
+        #walk all files/dirs in path
+        for file in os.listdir(path):
+            #loop files
+            rel_path = os.path.relpath(path + "/" + file)
+    
+            #get extra details
+            fl = pathlib.Path(rel_path)
+            date_modified = datetime.datetime.fromtimestamp(fl.stat().st_mtime)
+            file_size = fl.stat().st_size
+    
+            # add file to listing
+            if os.path.isdir(rel_path):
+                type = "folder"
+                hash = None
+            else:
+                type = "files"
+                hash = hash_file(rel_path)
+    
+            #add to listing
+            file_listing[rel_path] = [hash, date_modified, file_size, type]
+    
+            #if folders recursively loop contents
+            if type == "folder":
+                file_listing = self.list_dir_files_with_hash(rel_path, file_listing, recursive_folders)
+    
+        return file_listing
+    
+    def get_files(self):
+    
+        source_files = self.list_dir_files_with_hash(self.sourcePath)
+        self.source_files = {k.replace(self.sourcePath,""):v for k,v in source_files.items()}
+        dest_files = self.list_dir_files_with_hash(self.destPath)
+        self.dest_files = {k.replace(self.destPath, ""): v for k, v in dest_files.items()}
+    
+        pass
+    def sync_same_files(self):
+        """sync same file name changes to opposite folder"""
+        done = set()
+        for k,v in self.source_files.items():
+            
+            #if file found 
+            if k in self.dest_files:
+                dest_file = self.dest_files[k]
+                #if date modified greater in dest or file size
+                if dest_file[1] > v[1] or dest_file[2] > v[2]:
+                    self.log += "\n" + f"{k} updated in {self.sourcePath}"
+                    copyfile(self.destPath + k, self.soucePath + k)
+                    
+                #if date modified less in dest or file size
+                elif dest_file[1] > v[1] or dest_file[2] > v[2]:
+                    self.log += "\n" + f"{k} updated in {self.destPath}"
+                    copyfile(self.soucePath + k, self.destPath + k)
+                
+                #add changed files to set
+                done.add(k)
+                #remove done from the dictionary
+                del self.source_files[k]
+                del self.dest_files[k]
+                    
+        
 #     check_missing(source_files, dest_files,source,dest )
 #     check_needs_deleting(source_files, dest_files,source,dest)
 #
@@ -84,6 +117,8 @@ def sync_folders(source, dest):
 #
 
 if __name__ == "__main__":
-
+    
+    sync = Syncer("source/","dest/")
     while True:
-        sync_folders("source/","dest/")
+        sync.get_files()
+        sync.sync_same_files()
